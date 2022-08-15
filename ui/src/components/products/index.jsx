b860cabcc3be2d55059066f6fc761653ae7c6d45
Abdellah Allali <hi@allali.me>
2022-08-15 16:49:12 +0100

    [Update] add brand filter + image zoom on click

--- ui/src/components/products/index.jsx
+++ ui/src/components/products/index.jsx
@@ -1,39 +1,77 @@
 import React, { useEffect, useState } from "react";
 import "./Products.css";
-import { Button, Card, Row, Col, Table, Image} from "react-bootstrap";
+import {  Row, Col, Table, Image} from "react-bootstrap";
 import db from "./products.json";
 export default function Products() {
-  const [filter, setFilter] = useState("new");
-  const [originalData] = useState([...db])
-  const [data, updateData] = useState(originalData.filter((l) => l.statu === filter));
+  const [statuFilter, setStatuFilter] = useState("new");
+  const [brandFilter, setBrandFilter] = useState("")
+ 
+  const [data, updateData] = useState(db.filter((l) => l.statu === statuFilter && l.brand?.includes(brandFilter)));
+  const [brands, setBrands] = useState([])
+  useEffect(() => {
+    updateData(db.filter((l) => l.statu === statuFilter && (l.brand || "").includes(brandFilter)));
+ 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [statuFilter, brandFilter]);
 
   useEffect(() => {
-    updateData(originalData.filter((l) => l.statu === filter));
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [filter]);
+    let brandSet = new Set()
+    for (let i = 0; i < db.length; i++) {
+      const el = db[i]
+      if (el.brand) {
+        brandSet.add(el.brand)
+      }
+    }
+    
+    setBrands(Array.from(brandSet))
+  }, [])
+
+  function onClickImage(e) {
+    e.target.height = e.target.height === 500 ? 50 : 500
+  }
 
   return (
     <div className="inner-products">
       <Row>
-        <Col>
-          <h3> Select Filter  : </h3>
-          <h3> total products with that filter {data.length}</h3>
+        <Col className="col-2">
+          <h5> statu filter  : </h5>
         </Col>
         <Col>
-          <select   onChange={(e) => setFilter(e.target.value)}>
-          <option value="new">new</option>
+          <select   onChange={(e) => setStatuFilter(e.target.value)}>
+          <option value="new">new ({db.filter((el) => el.statu === "new").length})</option>
+            <option value="changed">Changed ({db.filter((el) => el.statu === "changed").length})</option>
+            <option value="stable">Stable ({db.filter((el) => el.statu === "stable").length})</option>
+            <option value="deleted">deleted ({db.filter((el) => el.statu === "deleted").length})</option>
 
-            <option value="changed">Changed</option>
-            <option value="stable">Stable</option>
-            <option value="deleted">deleted</option>
+          </select>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="col-2">
+          <h5> brand filter  : </h5>
+        </Col>
+        <Col>
+          <select onChange={(e) => setBrandFilter(e.target.value)}>
+          <option value="">all</option>
+         {brands.map((l, i) => {
+           return (<option key={i} value={l}>{l} ({db.filter((el) => el.statu === statuFilter && (el.brand || "").includes(l)).length})</option>)
+         })}
             
           </select>
         </Col>
       </Row>
       <Row>
+        <Col>
+
+          <h4> total products with that filter {data.length} (note: only 500 elements listed in table)</h4>
+        </Col>
+
+      </Row>
+      <Row>
       <Table striped bordered hover>
       <thead>
         <tr>
+          <th>#</th>
           <th>id</th>
           <th>image</th>
           <th>name</th>
@@ -47,7 +85,7 @@ export default function Products() {
       </thead>
       <tbody>
       
-        {data.map((l, i) => (
+        {data.slice(0,500).map((l, i) => (
           // <Card style={{ width: "15rem" }} key={l.id}>
           //   <Card.Img variant="top" src={l.image} />
           //   <Card.Body>
@@ -68,14 +106,16 @@ export default function Products() {
           //     <Button href={l.sourceCaregoryUrl} target="_blank" variant="primary" size="sm">Souce list</Button>
           //   </Card.Body>
           // </Card>
+          
             <tr key={i}>
+              <td>{i+1}</td>
             <td>{l.id}</td>
-            <td><Image src={l.image} width={50}></Image></td>
+            <td className="p-0"><Image src={l.image} height={50} onClick={onClickImage}></Image></td>
             <td>{l.name} - <a target="_blank" rel="noreferrer" href={l.url}>click</a></td>
           
             <td>{l.brand}</td>
             <td>{l.price.price}</td>
-            <td>{l.updated_at}</td>
+            <td className="text-nowrap">{l.updated_at}</td>
             <td><a target="_blank" rel="noreferrer" href={l.sourceCaregoryUrl}>click</a></td>
             <td>{l.index}</td>
            
