f3bb4bd7384e15917dff560388b8f3310d67c259
Abdellah Allali <hi@allali.me>
2022-09-03 14:54:49 +0100

    [Feat] utils folder

---
+++ scrapper/utils/__init__.py
@@ -0,0 +1,9 @@
+from .json_utils import *
+
+from .argsParser import parseArguments, showHelp
+
+from .jobLauncher import launchJob
+
+from .days_diff_calculator import days_between
+
+from .days_diff_calculator import *
