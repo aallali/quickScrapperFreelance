1dec774fbb95f0d97704bc6a3de653c17e9d98db
Abdellah Allali <hi@allali.me>
2022-09-13 18:45:57 +0100

    [fix] : check if url exists before fetch

--- ui/src/utils/useFetch.js
+++ ui/src/utils/useFetch.js
@@ -1,35 +1,35 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-const useFetch = url => {
-    const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(null);
-    const [error, setError] = useState(null);
-    
-    useEffect(() => {
-        setLoading('loading...')
-        setData([]);
-        setError(null);
-        const source = axios.CancelToken.source();
-        setTimeout(() => {
-            axios.get(url, { cancelToken: source.token })
-            .then(res => {
-              
-                setLoading(false);
-                res.data && setData(res.data);
-               
-            })
-            .catch(err => {
-                setLoading(false)
-                setError('An error occured. Awkward..')
-            })
-        }, 1000)
-        return () => {
-            source.cancel();
-        }
-    }, [url])
+const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(null);
+  const [error, setError] = useState(null);
 
-    return { data, loading, error }
-}
+  useEffect(() => {
+    setLoading("loading...");
+    setData([]);
+    setError(null);
+    const source = axios.CancelToken.source();
+    if (url)
+      setTimeout(() => {
+        axios
+          .get(url, { cancelToken: source.token })
+          .then((res) => {
+            setLoading(false);
+            res.data && setData(res.data);
+          })
+          .catch((err) => {
+            setLoading(false);
+            setError("An error occured. Awkward..");
+          });
+      }, 1000);
+    return () => {
+      source.cancel();
+    };
+  }, [url]);
+
+  return { data, loading, error };
+};
 
 export default useFetch;
